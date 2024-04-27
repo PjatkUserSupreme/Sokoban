0d47fde70d89ee60e5a875d5dae1489068217d6c
Oskar Rojek <s25723@pjwstk.edu.pl>
2024-04-27 14:26:56 +0200

    Created Commands and command Invoker

---
+++ Assets/Scripts/MoveLeftCommand.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftCommand : ICommand
+{
+    public bool Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Undo()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/MoveUpCommand.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveUpCommand : ICommand
+{
+    public bool Execute()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Undo()
+    {
+        throw new System.NotImplementedException();
+    }
+}
