784c02ca3839ef84119e23ed2e6484bfa143e93e
PjatkUserSupreme <s24817@pjwstk.edu.pl>
2024-04-27 12:55:42 +0200

    Player movement interface implemented

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour, PlayerControl.IMovementActions
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnUp(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDown(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnLeft(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnRight(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnUndo(InputAction.CallbackContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+}
