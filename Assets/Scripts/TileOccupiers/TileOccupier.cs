78cc5afcc446f28e016134f8631172c43c5fb8aa
PjatkUserSupreme <s24817@pjwstk.edu.pl>
2024-06-20 16:44:01 +0200

    Tile occupier docs

--- Assets/Scripts/TileOccupiers/TileOccupier.cs
+++ Assets/Scripts/TileOccupiers/TileOccupier.cs
@@ -1,13 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * Class representing an entity which can reside inside a tile object
+ */
 public class TileOccupier : MonoBehaviour
 {
     private int _coordX, _coordY;
     [SerializeField] private bool isPlayer;
     
-    
+    /**
+     * Setup the appropriate values based on TileScript script attached to its parent.
+     */
     public void Initialize()
     {
         TileScript tileScript = GetComponentInParent<TileScript>();
@@ -22,6 +25,11 @@ public class TileOccupier : MonoBehaviour
 
     public bool IsPlayer => isPlayer;
 
+    /**
+     * Moves the object to another tile object and changes values accordingly
+     *
+     * <param name="tileScript">Script attached to intended new parent object</param>
+     */
     public void MoveToTile(TileScript tileScript)
     {
         gameObject.transform.SetParent(tileScript.gameObject.transform);
